0c4622c6d9a0f22cd75afd529357131baccc8e2f
Marc <marcbaque1311@gmail.com>
2023-02-12 17:25:23 +0100

    [ ADD ] initial custom error type

--- finnhub/src/lib.rs
+++ finnhub/src/lib.rs
@@ -1,5 +1,16 @@
 use serde::Deserialize;
-use std::error::Error;
+
+#[derive(thiserror::Error, Debug)]
+pub enum FinnhubError {
+    #[error("Failed fetching the market news")]
+    MarktedNewsRequestFailed,
+    #[error("Failed parsing to ArticleMarketNews")]
+    ArticleMarketNewsParseFailed,
+
+    // General errors
+    #[error("Failed converting response to string")]
+    FailedResponseToString,
+}
 
 #[derive(Deserialize, Debug)]
 pub struct ArticleMarketNews {
@@ -14,12 +25,18 @@ pub struct ArticleMarketNews {
     url: String,
 }
 
-pub fn get_market_news(api_token: &str) -> Result<Vec<ArticleMarketNews>, Box<dyn Error>> {
+pub fn get_market_news(api_token: &str) -> Result<Vec<ArticleMarketNews>, FinnhubError> {
     let mut market_news_url =
         String::from("https://finnhub.io/api/v1/news?category=general&token=");
     market_news_url.push_str(api_token);
 
-    let response = ureq::get(&market_news_url).call()?.into_string()?;
-    let atricles = serde_json::from_str::<Vec<ArticleMarketNews>>(&response)?;
+    let response = ureq::get(&market_news_url)
+        .call()
+        .map_err(|_e| FinnhubError::MarktedNewsRequestFailed)?
+        .into_string()
+        .map_err(|_e| FinnhubError::FailedResponseToString)?;
+
+    let atricles = serde_json::from_str::<Vec<ArticleMarketNews>>(&response)
+        .map_err(|_e| FinnhubError::ArticleMarketNewsParseFailed)?;
     Ok(atricles)
 }
